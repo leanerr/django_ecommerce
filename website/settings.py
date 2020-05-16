adf3de4e6e46cc9920d42c493a173594bf7d159a
real_asgari@yahoo.com <real_asgari@yahoo.com>
2020-05-16 05:42:08 +0430

    first version:

--- website/settings.py
+++ website/settings.py
@@ -166,5 +166,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ali8asgari@gmail.com'
-EMAIL_HOST_PASSWORD = '20192019'
+EMAIL_HOST_USER = '...@gmail.com'
+EMAIL_HOST_PASSWORD = '...'
